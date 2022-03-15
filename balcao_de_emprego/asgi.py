5bd3da18f02992d4676393dd84b5fd2279ba0225
Luis Eduardo Cordeiro Salarini <eduardo.pmnf@gmail.com>
2022-03-15 13:09:04 -0300

    start django project

---
+++ balcao_de_emprego/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for balcao_de_emprego project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balcao_de_emprego.settings')
+
+application = get_asgi_application()
---
+++ balcao_de_emprego/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for balcao_de_emprego project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'balcao_de_emprego.settings')
+
+application = get_wsgi_application()
